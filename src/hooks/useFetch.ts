971ac677938c31e2f0af7b0a9bdbda22e99fdf34
wendson13 <wendson.dev@gmail.com>
2022-08-09 16:23:16 -0300

    adding request options in useFetch.

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,9 +1,10 @@
+import { AxiosRequestConfig } from 'axios';
 import useSWR from 'swr';
 import { api } from '../services/api';
 
-export function useFetch<T>(url: string, signal?: AbortSignal) {
+export function useFetch<T>(url: string, config?: AxiosRequestConfig) {
   const { data, error } = useSWR(url, async () => {
-    const { data } = await api.get<T>(url, { signal });
+    const { data } = await api.get<T>(url, config);
 
     return data;
   });
